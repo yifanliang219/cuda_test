9fc1b8cd67c0e63b93c7f94a9cb83d38c4286bfd
yifanliang219 <yifanliang219@gmail.com>
2026-05-05 20:24:27 +0100

    matrix mul check

--- src/main.cu
+++ src/main.cu
@@ -16,6 +16,24 @@ int main(int argc, char *argv[])
 
     const vector<float> A = loadMatrix("data/A_f32_K20_N512.npy", num, width);
     const vector<float> B = loadMatrix("data/B_f32_K20_N512.npy", num, width);
-    const vector<float> C = loadMatrix("data/B_f32_K20_N512.npy", num, width);
+    const vector<float> C = loadMatrix("data/C_f32_K20_N512.npy", num, width);
 
+    vector<float> firstRowInA(A.begin(), A.begin() + width);
+    vector<float> firstColInB(width);
+
+    for (int i = 0; i < width; i++)
+    {
+        firstColInB[i] = *(B.begin() + i * width);
+    }
+
+    float dot = 0.0;
+
+    for (int i = 0; i < width; i++)
+    {
+        dot += firstRowInA[i] * firstColInB[i];
+    }
+
+    printf("A * B = %.3f, C = %.3f\n", dot, C[0]);
+
+    return 0;
 }
