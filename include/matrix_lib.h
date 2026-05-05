d6361ef02cd63f5cc7ade1afb317a5dc6b3a85b3
yifanliang219 <yifanliang219@gmail.com>
2026-05-05 18:58:01 +0100

    matrix generator

--- include/matrix_lib.h
+++ include/matrix_lib.h
@@ -5,3 +5,45 @@
 #include <string>
 #include <cnpy.h>
 
+using namespace std;
+using namespace cnpy;
+
+vector<float> generateMatrices(size_t num, size_t width, int seed)
+{
+    mt19937 rng(seed);
+    uniform_real_distribution<float> dist(0.0f, 1.0f);
+
+    vector<float> data(num * width * width);
+
+    for (float &x : data)
+    {
+        x = dist(rng);
+    }
+
+    return data;
+}
+
+void saveMatrix(
+    const string &path,
+    const vector<float> &data,
+    size_t num,
+    size_t width)
+{
+    vector<size_t> shape = {num, width, width};
+    npy_save(path, data.data(), shape, "w");
+}
+
+vector<float> loadMatrix(
+    const string &filename,
+    size_t &num,
+    size_t &width)
+{
+    NpyArray arr = npy_load(filename);
+
+    num = arr.shape[0];
+    width = arr.shape[1];
+
+    float *ptr = arr.data<float>();
+
+    return vector<float>(ptr, ptr + arr.num_vals);
+}
--- src/main.cu
+++ src/main.cu
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "cxtimers.h"
-#include "cuda_runtime.h"
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
 #include "thrust/device_vector.h"
 #include "image_lib.h"
-#include "chp3_gray.cuh"
+#include "matrix_lib.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string path = "data/pikachu.png";
-    Image img_in = loadImageAsUnsignedCharVector(path);
-    printf("Input image: height %d, width: %d, channels: %d, size: %d\n", img_in.height, img_in.width, img_in.channels, (int)img_in.data.size());
+    size_t num = 20;
+    size_t width = 512;
 
-    Image img_same = Image{img_in.height, img_in.width, img_in.channels, img_in.data};
-    saveImageAsPng(img_same, "data/pikachu_same.png");
-    printf("Same image: height %d, width: %d, channels: %d, size: %d\n", img_same.height, img_same.width, img_same.channels, (int)img_same.data.size());
+    vector<float> A = loadMatrix("A_f32_K20_N512.npy", num, width);
+    vector<float> B = loadMatrix("B_f32_K20_N512.npy", num, width);
 
-    vector<unsigned char> gray = colorToGray(img_in.data, img_in.height, img_in.width);
-    Image img_out = Image{img_in.height, img_in.width, 1, move(gray)};
-    printf("Output image: height %d, width: %d, channels: %d, size: %d\n", img_out.height, img_out.width, img_out.channels, (int)img_out.data.size());
-    saveImageAsPng(img_out, "data/pikachu_gray.png");
-    return 0;
 }
